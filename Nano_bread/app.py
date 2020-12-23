ea0d45c6b1954ddc73016c9d5d7a1c66a73609da
聞承剛 <john.wen@ezchain.net>
2020-12-23 15:08:03 +0800

    web develop init

--- Nano_bread/app.py
+++ Nano_bread/app.py
@@ -1,9 +1,28 @@
-from flask import Flask, request
-import yolov4_app_1_4
+from flask import Flask, request, render_template, request, jsonify, url_for, redirect
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+# import yolov4_app_1_4
 import json
 import numpy as np
+from os import listdir, getcwd, chdir, rename, remove
+from os.path import isdir, isfile, getctime
+import cv2
+import base64
+import random
+import string
+from bs4 import BeautifulSoup
+
+PREBASE64 = "data:image/jpeg;base64,"
+USERFILE  = 'root'
+CUSTOMER_FILE = "customer/"
+
+
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 
 @app.route('/bread/query/category/v1', methods=['POST'])
@@ -122,6 +141,227 @@ def confidence():
         }
         return rtn_data
 
+
+
+@app.route('/')
+def first_page():
+    """
+    頁面最初的頁面
+    :return:
+    """
+    return render_template('first_page.html')
+
+@app.route('/recognitionPage', methods=['GET'])
+def recognitionpage():
+    """
+    回傳辨識HTML
+    :return:
+    """
+    imga = cv2.imread('static/A.png')
+    basea = cv2_strbase64(imga)
+    return render_template('recognition.html', basea=basea)
+
+
+@app.route('/customer_productData')
+def customer_productData():
+    username = USERFILE
+    alllist = listdir(CUSTOMER_FILE+str(username))
+    html = html_customer_productMaker(username, alllist)
+    return html
+
+@app.route('/customer_editProductData', methods=['POST'])
+def customer_editProductData():
+    username = USERFILE
+    data = request.get_json()
+    product_id = data["product_id"]
+    # print("os path :", getcwd())
+    html = html_editcustomer_productMaker(username, product_id)
+    return html
+
+@app.route('/customer_add_product')
+def customer_add_product():
+    username = USERFILE
+    html = html_add_customer_product_maker(username)
+    return html
+
+def html_add_customer_product_maker(customername):
+    """
+    產生客戶新增產品profile的html
+    :param customername:
+    :param productId:
+    :return:
+    """
+    url = "templates/editProductProfile.html"
+    soup_table = BeautifulSoup(open(url), 'html.parser')
+    img_path = 'static/A.png'
+    img = cv2.imread(img_path)
+    img_src = cv2_strbase64(img)
+    dom_body = soup_table.find(id="blsh1")
+    dom_body["src"] = img_src
+
+    output = soup_table.prettify()
+    return output
+
+
+
+
+def html_editcustomer_productMaker(customername, productId):
+    """
+    產生客戶編輯產品profile的html
+    :param customername:
+    :param productId:
+    :return:
+    """
+    url = "templates/editProductProfile.html"
+    soup_table = BeautifulSoup(open(url), 'html.parser')
+    with open('customer/' + customername + '/' + productId + '/data.data') as json_file:
+        data = json.load(json_file)
+        img_path = 'customer/' + customername + '/' + productId + '/title.jpg'
+        img = cv2.imread(img_path)
+        img_src = cv2_strbase64(img)
+        dom_body = soup_table.find(id="blsh1")
+        dom_body["src"] = img_src
+        dom_body = soup_table.find(id="pre_barcode_error")
+        dom_body["value"] = data["product_id"]
+        dom_body = soup_table.find(id="barcode_error")
+        dom_body["value"] = data["product_id"]
+        dom_body = soup_table.find(id="product_name_error")
+        dom_body["value"] = data["product_name"]
+        dom_body = soup_table.find(id="remarks_error")
+        dom_body["value"] = data["memo"]
+
+    output = soup_table.prettify()
+    return output
+
+def html_customer_productMaker(customername, this_list):
+    """
+    :param customername:
+    :param this_list:
+    :return:
+    """
+    url = "templates/customerProductTable.html"
+    soup_table = BeautifulSoup(open(url), 'html.parser')
+    soup = BeautifulSoup()
+
+    for i, name in enumerate(this_list):
+        if isfile("customer/"+customername+"/"+name):
+            continue
+        data_url = "customer/"+customername+"/"+name+"/data.data"
+        url = "templates/tr_tlp.html"
+        soup_body = BeautifulSoup(open(url), 'html.parser')
+        with open(data_url) as json_file:
+            flag = i + 1
+            img_url = "customer/"+customername+"/"+name+"/title.jpg"
+            img = cv2.imread(img_url)
+            img_base64 = cv2_strbase64(img)
+            data = json.load(json_file)
+            bodys = soup_body.find_all('th')
+            newtag = soup.new_tag(name="input", type="checkbox", id="checkbox_" + str(flag))
+            bodys[0].append(newtag)
+            bodys[1].string = data["product_id"]
+            bodys[1]["id"]  = "pdid_"+str(flag)
+            newtag = soup.new_tag(name="img", src=img_base64, height="70")
+            bodys[2].append(newtag)
+            bodys[3].string = data["product_name"]
+            bodys[4].string = data["memo"]
+            bodys[5].string = data["image_count"]
+            if data["ON"] == "1":
+                bodys[6].string = "ON"
+            else:
+                bodys[6].string = "OFF"
+            newtag = soup.new_tag(name="button",
+                                  attrs={"class": "btn btn-outline-danger",
+                                         "type": "button",
+                                         "data-productID": data["product_id"],
+                                         "onclick": "showCustomerEditProduct(this)",
+                                         "id": "button_%i_1" % flag})
+            newtag.string = "編輯"
+            bodys[7].append(newtag)
+            newtag = soup.new_tag(name="button",
+                                  attrs={"class": "btn btn-outline-danger",
+                                         "type": "button",
+                                         "data-productID": data["product_id"],
+                                         "onclick": "showCustomerProductsImage(this)",
+                                         "id": "button_%i_2" % flag})
+            newtag.string = "圖資"
+            bodys[7].append(newtag)
+            soup_table.tbody.append(soup_body)
+    output = soup_table.prettify()
+    return output
+
+
+
+
+def imgSrc2_cv2img(src):
+    """
+    去除圖片base64多餘前綴詞並轉化成OPENCV的IMG型態
+    :param src:
+    :return:
+    """
+    src = src.split(",")[1]
+    img = base64toimg(src)
+    return img
+
+
+def base64toimg(string):
+    """
+    base64 to np type image
+
+    :param string: base64 string
+    :return: numpy image
+    """
+    erro  = True
+    lens = len(string)
+    lenx = lens - (lens % 4 if lens % 4 else 0)
+    result = string[:lenx]
+    try:
+        base64encode = bytes(result, encoding="utf8")
+        base64decode = base64.b64decode(base64encode)
+        img_array = np.fromstring(base64decode, np.uint8)
+        img = cv2.imdecode(img_array, cv2.COLOR_RGB2BGR)
+        if img is None:
+            return None
+        else:
+            return img
+    except Exception as e:
+        return None
+
+
+def cv2_strbase64(image):
+    base64_str = cv2.imencode('.jpg', image)[1].tostring()
+    base64_str = base64.b64encode(base64_str)
+    output = PREBASE64+str(base64_str).split("'")[1]
+    return output
+
+
+def get_ipc(data):
+    """
+    透過opencv取得RTSP串流攝影機當下影像
+    :param data: JSON格式 "ip": RTSP的IP
+    :return:成功回傳JSON "ret":True , "base": 影像的base64
+            失敗回傳JSON "ret":False
+    """
+    ip = data["ip"]
+    try:
+        cap = cv2.VideoCapture(ip)
+        ret, image = cap.read()
+        if ret:
+            out_base = cv2_strbase64(image)
+            # print("backend spend %f" % (end - start))
+            return {"ret": ret, "base": out_base}
+        # print("backend spend %f" % (end - start))
+        return {"ret": ret}
+    except Exception as e:
+        return {"ret": False}
+
+def generate_random_str(randomlength):
+    '''
+    string.digits = 0123456789
+    string.ascii_letters = 26个小写,26个大写
+    '''
+    str_list = random.sample(string.digits + string.ascii_letters, randomlength)
+    random_str = ''.join(str_list)
+    return random_str
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ Nano_bread/static/first_page.js
@@ -0,0 +1,315 @@
+let uploadImageBase64List = [];
+var filesizeLimit = 6 * 1024 * 1024
+
+$("#syBt_3").click(function(){
+    showTable(1)
+});
+$("#tb1_bt1").click(function(){
+    // 呈現客戶所有產品清單頁面
+    showTable(2)
+    showCustomerProductList()
+});
+$("#customerUploadImg").change(function(){
+    $("#uploadLayout").empty();
+    readUrlMultiple(this)
+});
+
+
+function imageAjax(srcs){
+    var value = 'Bearer '+localStorage.getItem("myJWT");
+    [].forEach.call(srcs,src =>{
+        $.ajax({
+            url:"/save_product_image",
+            async:false,
+            method: "post",
+            contentType: "application/json;charset=UTF-8",
+            dataType:"json",
+            data:JSON.stringify({"base64":src,"product_id":productIdSend()}),
+            beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', value);
+            },
+        }).done(function(data){
+            console.log("good job");
+        });
+    });
+
+
+}
+
+
+async function productImageSave(obj){
+    var srcArray = [];
+    let imgArray = $("#uploadLayout > li > div > div > img");
+    [].forEach.call(imgArray, imgObj =>{
+        srcArray.push(imgObj.getAttribute('src'))
+    });
+    if (srcArray.length ===0){alert("沒有上傳照片！");return ""}
+    await imageAjax(srcArray);
+
+    // close modal and return to product_id images page
+
+    alert("上傳成功！");
+
+    $("#uploadLayout").empty();
+    showCustomerProductsImage(obj);
+    $("#modal1").modal('hide');
+
+}
+
+function closeModal(){
+    $("#uploadLayout").empty();
+    $("p[class='f-l ml-3']").text("選擇圖片0張");
+}
+
+function removeUploadImage(obj){
+    var id = 0;
+    id = parseInt(obj.getAttribute("data-key"));
+    $("#uploadLayout > li[data-key='"+id.toString()+"']").remove();
+    let imageCount = getNum($("p[class='f-l ml-3']").text());
+    $("p[class='f-l ml-3']").text("選擇圖片"+(imageCount-1).toString()+"張")
+}
+
+function readUrlMultiple(input) {
+    $("p[class='f-l ml-3']").text("選擇圖片0張");
+    var inputt = input;
+    var srcList = [];
+    uploadImageBase64List.length = 0;
+    let files = input.files;
+    let imgCount = 0;
+    function readAndPreview(file) {
+        var reader = new FileReader();
+        reader.addEventListener("load", function () {
+        // convert image file to base64 string
+        // preview.src = reader.result;
+
+            function imageHtmlMaker(number,obj){
+                src  = obj.result;
+                uploadImageBase64List.push(src);
+                liHtml = '<li class="layout-3-3 my-3 px-3" data-key="'+number.toString()+'">'+
+                       '<div class="select-img-wrapper is-valid">'+
+                       '<div>'+
+                       '<span class="bt-X" data-key="'+number.toString()+ '" onClick="removeUploadImage(this)"></span>'+
+                       '<img data-key="'+number.toString()+ '" height="150" width="100%" src="'+src+'">'+
+                       '</div>'+
+                       '</div>'+
+                       '</li>'
+                $("#uploadLayout").append(liHtml);
+            }
+
+            imageHtmlMaker(imgCount,this);
+            imgCount += 1;
+            $("p[class='f-l ml-3']").text("選擇圖片"+imgCount.toString()+"張")
+
+        }, false);
+        reader.readAsDataURL(file);
+    }
+
+    if (files) {
+        [].forEach.call(files, readAndPreview);
+    }
+
+
+}
+
+function goRecognitionPage(){
+    window.location.href='recognitionPage';
+}
+
+function backToCustomerProductList(){
+    // 返回客戶所有產品清單頁面
+    showTable(2)
+    showCustomerProductList()
+}
+
+async function deleteCustomerProduct(obj){
+    var deleteArray = [];
+    var productId = obj.getAttribute("data-productid")
+    var inputs = $("input[data-art5='input']");
+    [].forEach.call(inputs, input =>{
+        if (input.checked == true){
+            deleteArray.push(input.getAttribute("data-path"))
+        }
+    });
+
+    function ajax_(dataArray,id){
+
+        var value = 'Bearer '+localStorage.getItem("myJWT");
+        [].forEach.call(dataArray,fileName =>{
+            var payload = {"file_path":fileName,
+                           "product_id":id};
+            $.ajax({
+            url:"/delete_product_image",
+            method: "post",
+            contentType: "application/json;charset=UTF-8",
+            dataType:"json",
+            data:JSON.stringify(payload),
+            beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', value);
+            },
+            }).done(function(data){
+            console.log("good job");
+            });
+
+        });
+    }
+
+    await ajax_(deleteArray,productId);
+
+    alert("照片刪除成功！");
+    showCustomerProductsImage(obj);
+    var x = 1;
+}
+
+function saveEditCustomerProfile(){
+    var preProductId = $("#pre_barcode_error").val();
+    var productId = $("#barcode_error").val();
+    var productName = $("#product_name_error").val();
+    var memo = $("#remarks_error").val();
+    var imgscr = $("#blsh1").attr("src")
+    inputdata = {"product_id":productId,
+                 "pre_product_id":preProductId,
+                 "product_name":productName,
+                 "memo":memo,
+                 "img":imgscr
+    }
+    $.ajax({
+        url:"customer_saveProductData",
+        method: "post",
+        contentType: "application/json;charset=UTF-8",
+        dataType:"html",
+        data:JSON.stringify(inputdata),
+        beforeSend: function (xhr) {
+        var value = 'Bearer '+localStorage.getItem("myJWT")
+        xhr.setRequestHeader('Authorization', value);
+        },
+        success: function (data) {
+            showTable(2)
+            showCustomerProductList()
+        },
+        error: function (xhr,status,error) {
+            errorBackToLogIn(xhr)
+        },
+    });
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        if (filesizeLimit <= input.files[0].size) {
+            alert("圖片檔案無法上傳大於2MB");
+        }
+        else{
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#blsh1').attr('src', e.target.result);
+            }
+            var filename = input.files[0].name.split(".",1)[0]
+            reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+    }
+}
+
+function showTable(tableId){
+    var a = $("[id^='table-']")
+    for (i=0;i<a.length;i++){
+        if (a[i].id.split("-")[a[i].id.split("-").length-1]==tableId){
+            a[i].style.display="block"
+        }
+        else{
+            a[i].style.display="none"
+        }
+    }
+}
+
+function showCustomerProductList(){
+    var value = 'Bearer '+localStorage.getItem("myJWT")
+    $.ajax({
+        url:"customer_productData",
+        method: "get",
+        contentType: "application/json;charset=UTF-8",
+        dataType:"html",
+        beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', value);
+        },
+        success: function (data) {
+            $("#table1").html(data)
+        },
+        error: function (xhr,status,error) {
+            errorBackToLogIn(xhr)
+        },
+    });
+}
+
+function productIdSend(){
+    id = $("h4[class='float-r m-t-5']").attr("data-id");
+    return id
+}
+
+function showCustomerEditProduct(obj){
+//    var id = obj.id.split("_")[1];
+    var value = 'Bearer '+localStorage.getItem("myJWT");
+    var productId = obj.getAttribute("data-productID");
+    inputdata = {"product_id":productId}
+    $.ajax({
+        url:"/customer_editProductData",
+        method: "post",
+        contentType: "application/json;charset=UTF-8",
+        dataType:"html",
+        data:JSON.stringify(inputdata),
+        beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', value);
+        },
+        success: function (data) {
+            showTable(5);
+            $("#table-5").html(data);
+        },
+        error: function (xhr,status,error) {
+                errorBackToLogIn(xhr);
+        },
+    });
+}
+
+function passProductId(obj){
+    var productId = obj.getAttribute("data-productID");
+    $("#imageCount").text(obj.getAttribute("data-imageCount"));
+    $("#bt_saveImage").attr("data-productID", productId);
+}
+
+function showCustomerProductsImage(obj){
+//    var id = obj.id.split("_")[1];
+    var value = 'Bearer '+localStorage.getItem("myJWT");
+    var productId = obj.getAttribute("data-productid");
+    inputdata = {"product_id":productId}
+    $.ajax({
+        url:"/reveal_customer_productdataimg",
+        method: "post",
+        contentType: "application/json;charset=UTF-8",
+        dataType:"html",
+        data:JSON.stringify(inputdata),
+        beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', value);
+        },
+        success: function (data) {
+            showTable(6)
+            $("#table-6").html(data)
+        },
+        error: function (xhr,status,error) {
+                errorBackToLogIn(xhr)
+        },
+    });
+
+}
+
+
+function errorBackToLogIn(xhr){
+    if (xhr["status"]=="422" || xhr["status"]=="401" ){
+            alert("Your Authorization is expired or invalid, please sign in again!");
+            localStorage.clear();
+            window.location.href='/';
+       }
+}
+
+function getNum(text){
+    // only get number and English letter
+	var value = text.replace(/[^0-9a-zA-Z]/ig,"");
+	return value;
+}
