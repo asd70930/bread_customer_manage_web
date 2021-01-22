0e4502350993e89505e708af10adebf8afe5d73f
聞承剛 <john.wen@ezchain.net>
2021-01-22 15:44:08 +0800

    Fix, 合併前的調整

--- Nano_bread/app.py
+++ Nano_bread/app.py
@@ -34,121 +34,6 @@ moment = Moment(app)
 check_root_file()
 
 
-@app.route('/bread/query/category/v1', methods=['POST'])
-def category():
-    if request.method == 'POST':
-        data = request.json
-        b64 = data['values']['b64string']
-        name = data['values']['name']
-        if data['function'] == 'category':
-            image = yolov4_app_1_4.isimage(b64)
-            if image:
-                result = yolov4_app_1_4.inference(b64, 1)
-                result_data = result['data']
-                if len(result_data) != 1:
-                    rtn_data = {
-                        "rtn": 405,
-                    }
-                else:
-                    rtn_data = {
-                        "rtn": 200,
-                        "name": result_data[0],
-                        "id": result_data[1]
-                    }
-                return rtn_data
-            else:
-                rtn_data = {
-                    "rtn": 500,
-                }
-                return rtn_data
-    else:
-        rtn_data = {
-            "rtn": 404
-        }
-        return rtn_data
-
-
-@app.route('/bread/query/quantity/v1', methods=['POST'])
-def quantity():
-    if request.method == 'POST':
-        data = request.json
-        b64 = data['values']['b64string']
-        if data['function'] == 'quantity':
-            image = yolov4_app_1_4.isimage(b64)
-            if image:
-                result = yolov4_app_1_4.inference(b64, 1)
-                result_data = result['data']
-                rtn_data = {
-                    "rtn": 200,
-                    "rtn_values": result_data
-                }
-                return rtn_data
-            else:
-                rtn_data = {
-                    "rtn": 500,
-                }
-                return rtn_data
-    else:
-        rtn_data = {
-            "rtn": 404
-        }
-        return rtn_data
-
-
-@app.route('/bread/query/position/v1', methods=['POST'])
-def position():
-    if request.method == 'POST':
-        data = request.json
-        if data['function'] == 'position':
-            b64 = data['values']['b64string']
-            image = yolov4_app_1_4.isimage(b64)
-            if image:
-                result = yolov4_app_1_4.inference(b64, 3)
-                result_data = result['data']
-                rtn_data = {
-                    "rtn": 200,
-                    "b64string": result['b64string'],
-                    "rtn_values": result_data
-                }
-                return json.dumps(rtn_data)
-            else:
-                rtn_data = {
-                    "rtn": 500,
-                }
-                return rtn_data
-    else:
-        rtn_data = {
-            "rtn": 404
-        }
-        return rtn_data
-
-
-@app.route('/bread/query/confidence/v1', methods=['POST'])
-def confidence():
-    if request.method == 'POST':
-        data = request.json
-        if data['function'] == 'confidence':
-            b64 = data['values']['b64string']
-            image = yolov4_app_1_4.isimage(b64)
-            if image:
-                result = yolov4_app_1_4.inference(b64, 4)
-                result_data = result['data']
-                rtn_data = {
-                    "rtn": 200,
-                    "b64string": result['b64string'],
-                    "rtn_values": result_data
-                }
-                return rtn_data
-            else:
-                rtn_data = {
-                    "rtn": 500,
-                }
-                return rtn_data
-    else:
-        rtn_data = {
-            "rtn": 404
-        }
-        return rtn_data
 
 
 @app.route('/show_ipc', methods=['POST'])
@@ -183,9 +68,6 @@ def cameraSetting():
     :return:
     """
     return render_template('setCamera.html')
-
-
-
 
 
 @app.route('/recognitionPage', methods=['GET'])
